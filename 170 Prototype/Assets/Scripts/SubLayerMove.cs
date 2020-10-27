e578717fa7858da2f340d4091ae297caaf1eeda8
Saif <skausar@ucsc.edu>
2020-10-27 11:01:45 -0700

    First Prototype

---
+++ 170 Prototype/Assets/Scripts/SubLayerMove.cs
@@ -0,0 +1,61 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SubLayerMove : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public Camera cam;
+    public GameObject subLayer1;
+    public GameObject subLayer2;
+
+    Vector3 mainScene = new Vector3(0,0,0);
+    Vector3 subStart1;
+    Vector3 subStart2;
+
+    float move1 = 0f;
+    float move2 = 0f;
+
+    // Update is called once per frame
+    private void Update()
+    {
+      if(Input.GetButtonDown("ShowMap") && cam.orthographicSize == 10f){
+        rb.constraints = RigidbodyConstraints2D.FreezePositionX;
+        cam.orthographicSize = 35f;
+      }
+      else if(move1 != 0f)
+      {
+        subLayer1.transform.Translate(0,move1/2f,0);
+        if(subLayer1.transform.position == mainScene || subLayer1.transform.position == subStart1){
+          move1 = 0f;
+        }
+      }
+      else if(move2 != 0f){
+        subLayer2.transform.Translate(0,move2/2f,0);
+        if(subLayer2.transform.position == mainScene || subLayer2.transform.position == subStart2){
+          move2 = 0f;
+        }
+      }
+      else if(cam.orthographicSize == 35f && Input.GetButtonDown("ShowMap")){
+        rb.constraints = RigidbodyConstraints2D.None;
+        rb.constraints = RigidbodyConstraints2D.FreezeRotation;
+        cam.orthographicSize = 10f;
+      }
+      else if(cam.orthographicSize == 35f){
+        if(subLayer1.transform.position == mainScene && Input.GetKeyDown(KeyCode.C)){
+          move1 = -0.25f;
+        }
+        else if(Input.GetKeyDown(KeyCode.C)){
+          subStart1 =  subLayer1.transform.position;
+          move1 = 0.25f;
+        }
+        if(subLayer2.transform.position == mainScene && Input.GetKeyDown(KeyCode.X)){
+          move2 = 0.25f;
+        }
+        else if(Input.GetKeyDown(KeyCode.X)){
+          subStart2 =  subLayer2.transform.position;
+          move2 = -0.25f;
+        }
+      }
+    }
+}
---
+++ 170 Prototype/Assets/Scripts/isSubLayerMoving.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class isSubLayerMoving : MonoBehaviour
+{
+    public GameObject layer;
+    float initialy;
+    // Update is called once per frame
+    void Start(){
+      initialy = layer.transform.position.y;
+    }
+    void Update()
+    {
+      if(layer.transform.position.y != 0f && layer.transform.position.y != initialy){
+        foreach (Transform t in layer.transform) {
+          t.GetComponent<BoxCollider2D>().enabled = false;
+        }
+      }
+      else{
+        foreach (Transform t in layer.transform) {
+          t.GetComponent<BoxCollider2D>().enabled = true;
+        }
+      }
+    }
+}
