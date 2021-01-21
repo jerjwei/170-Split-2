c58de74be775d39b23f66a0799456fd3bb1392f9
Bowen Zhao <827347089@qq.com>
2021-01-21 01:57:17 -0500

    monster on ground, slope. Bounce on air wall

---
+++ 170 Prototype/Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+
+    public Rigidbody2D rb;
+
+    public int enemySpeed = 3;
+    public int horizontalDirection;
+    //public float bounceDistance = 0.7f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        /*RaycastHit2D hit = Physics2D.Raycast(transform.position, new Vector2(horizontalDirection, 0));*/
+        rb.velocity = new Vector2(horizontalDirection * enemySpeed, rb.velocity.y);
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D col)
+    {
+        // if collide with Air Wall
+        if(col.tag == "Air_Wall"){
+            Flip();
+        }
+
+    }
+
+    //turn the horizontal direction to the other way
+    void Flip()
+    {
+        if(horizontalDirection > 0)
+        {
+            horizontalDirection = -1;
+        }
+        else
+        {
+            horizontalDirection = 1;
+        }
+
+
+    }
+}
